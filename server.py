769db775b29b0a1789dea852b96202efb2b687d1
Bruno <brunophilippsen266@gmail.com>
2023-08-31 17:12:31 -0300

    Simlpesmente a primeira solucao que eu tinha usado funcionou. Por que não deu certo da primeira vez? Porque eu, em vez de chamar cada elemento da lista e executar o método em cima, tentei executar na var conn da funcao controladora em si. Literalmente eu tenho a solução pra isso fazem uns dois dias. Pelo lado bom eu me obriguei a estudar um pouco de Queue e a lib threading. tb  entendi um pouco mais sobre classe e métodos.

--- server.py
+++ server.py
@@ -14,13 +14,18 @@ def chat_client(conn, addr): # precisaria rodar múltiplas instâncias dessa fun
                 
                 if message: # aqui provavelmente um if pra checar se a msg não é comando "@"
                     print(f"<{addr}>: {message}")
-                    conn.send(f"<{addr}> {message}".encode("utf-8"))
-                    # for user in connectedUsers: conn.send(f"{addr}{message}".enconde("utf-8"))
-                    # nao funciona
+                    conn.send(f"<Voce: {addr}> {message}".encode("utf-8"))
+                    for user in connectedUsers:
+                         if user != conn:
+                            try:
+                              user.send(f"<User: {addr}> says: {message}".encode("utf-8"))
+                            except:
+                                connectedUsers.remove(user)
+                                user.close()
                 else:
                     client_connected = False
+                    connectedUsers.remove(conn)
                     print(f"DEBUG: {addr} desconectado.")
-
 
     except Exception as ex:
         print("ERROR: ", ex)
@@ -34,12 +39,13 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(('0.0.0.0', port)) # liga o socket a um endereço específico,
 server.listen(5) # espera pra entrar
 
+connectedUsers = list()
+
 running = True
 while running:
     conn, addr = server.accept() # aceita a conexão, porém pausa o código until alguma conexão é aceita
     print(f"DEBUG: {addr} conectado")
-    # connectedUsers = []
-    # connectedUsers.append(conn)
+    connectedUsers.append(conn)
     t1 = Thread(target=chat_client, args=(conn, addr))
     t1.start() # como comunicar todas as threads entre si?
     # var global?
